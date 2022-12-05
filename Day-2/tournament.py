65f662b1c0a278a8ace930e4f60b861adcbc4687
OttoMP <otto28mai@gmail.com>
2022-12-05 14:35:32 -0300

    Day 2 Part 2 completed

--- Day-2/tournament.py
+++ Day-2/tournament.py
@@ -4,6 +4,7 @@ from pprint import pprint
 def main():
     file1 = open("guide.txt", 'r')
 
+    print("Part 1")
     score = 0
     for line in file1:
         his_game = line.split()[0]
@@ -32,6 +33,41 @@ def main():
                 score += 6
             elif his_game == "C":
                 score += 3
+
+    # Closing files
+    file1.close()
+    print("My final score is {}".format(score))
+
+    print("Part 2")
+    file1 = open("guide.txt", 'r')
+    score = 0
+    for line in file1:
+        his_game = line.split()[0]
+        my_game = line.split()[1]
+        if my_game == "X":
+            score += 0
+            if his_game == "A":
+                score += 3
+            elif his_game == "B":
+                score += 1
+            elif his_game == "C":
+                score += 2
+        elif my_game == "Y":
+            score += 3
+            if his_game == "A":
+                score += 1
+            elif his_game == "B":
+                score += 2
+            elif his_game == "C":
+                score += 3
+        elif my_game == "Z":
+            score += 6
+            if his_game == "A":
+                score += 2
+            elif his_game == "B":
+                score += 3
+            elif his_game == "C":
+                score += 1
 
     # Closing files
     file1.close()
